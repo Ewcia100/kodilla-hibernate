f56f1ba8d1eb3d8944d087f6eafbb09107b6adf8
Ewcia100 <epawelska@gmail.com>
2018-01-11 11:21:37 +0100

    13.4_NamedQuery

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/dao/EmployeeDao.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/dao/EmployeeDao.java
@@ -1,6 +1,7 @@
 package com.kodilla.hibernate.manytomany.dao;
 
-import com.kodilla.hibernate.manytomany.Company;
+
+import com.kodilla.hibernate.manytomany.Employee;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -11,7 +12,7 @@ import java.util.List;
 
 @Transactional
 @Repository
-public interface CompanyDao extends CrudRepository<Company, Integer> {
-@Query(nativeQuery = true)
-    List<Company>retrieveWithThreeFirstLetters(@Param("PARTNAME") String partName);
+public interface EmployeeDao extends CrudRepository<Employee, Integer> {
+    @Query
+    List<Employee> retrieveWithLastname(@Param("LASTNAME") String lastname);
 }
